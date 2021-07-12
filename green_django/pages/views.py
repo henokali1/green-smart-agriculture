af9b058b7e6361df3c70ccbc223e3703bff24272
henokali1@gmail.com <henokali1@gmail.com>
2021-07-12 03:43:21 +0000

    navbar bottom fixed

--- green_django/pages/views.py
+++ green_django/pages/views.py
@@ -1,5 +1,20 @@
 from django.views.generic import TemplateView
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import *
+import ast
+from time import time
+from datetime import datetime, timedelta
+from django.db.models import Sum
+import json
+import pickle
+import random
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+def t(request):
+    args={}
+    return render(request, 'pages/t.html', args)
