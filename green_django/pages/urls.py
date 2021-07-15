779b89ef109cff658f5b3da235af412efde00d24
henokali1@gmail.com <henokali1@gmail.com>
2021-07-15 07:04:04 +0000

    live sensor data page

--- green_django/pages/urls.py
+++ green_django/pages/urls.py
@@ -4,6 +4,7 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('sensor-real-time/', views.sensor_real_time, name='sensor-real-time.html'),
     path('t/', views.t, name='t.html'),
     path('dashboard/', views.dashboard, name='dashboard.html'),
 ]
--- green_django/pages/views.py
+++ green_django/pages/views.py
@@ -18,11 +18,13 @@ class HomePageView(TemplateView):
 
 @login_required
 def dashboard(request):
-	# dashboard = [{'url': 'url', 'icon': 'home', 'title': 'Sensor Data'}, {'url': 'url', 'icon': 'edit', 'title': 'Sensor Data'}, {'url': 'url', 'icon': 'dashboard', 'title': 'Sensor Data'}]
 	args = {'dashboard': Dashboard.objects.all()}
 	return render(request, 'pages/dashboard.html', args)
 
+def sensor_real_time(request):
+	args = {}
+	return render(request, 'pages/sensor-real-time.html', args)
+
 def t(request):
-	# dashboard = [{'url': 'url', 'icon': 'home', 'title': 'Sensor Data'}, {'url': 'url', 'icon': 'edit', 'title': 'Sensor Data'}, {'url': 'url', 'icon': 'dashboard', 'title': 'Sensor Data'}]
 	args = {'dashboard': Dashboard.objects.all()}
 	return render(request, 'pages/t.html', args)
